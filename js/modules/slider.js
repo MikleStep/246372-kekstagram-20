331cf8315116066bd4a8d6eec4b0a3127c90e360
MikleStep <raiving_rabbid@mail.ru>
2020-06-28 21:05:39 +0300

    Исправлена функция слайдера

--- js/modules/slider.js
+++ js/modules/slider.js
@@ -2,36 +2,34 @@
 window.slider = (function () {
   return {
     dragSlider: function (pin, line, depth) {
+      var isPinPassed;
+      var movePin = function (evt) {
+        var limitMovementX = {
+          min: line.offsetLeft - 20,
+          max: line.offsetLeft + line.offsetWidth - pin.offsetWidth
+        };
+        var pinCoord = pin.offsetLeft + evt.movementX;
+        if (pinCoord < limitMovementX.min) {
+          pinCoord = limitMovementX.min;
+        }
+        if (pinCoord > limitMovementX.max) {
+          pinCoord = limitMovementX.max;
+        }
+        pin.style.left = pinCoord + 'px';
+        depth.style.width = pinCoord + 'px';
+      };
       pin.addEventListener('mousedown', function () {
-        var movePin = function (evt) {
-          var limitMovementX = {
-            min: line.offsetLeft - 20,
-            max: line.offsetLeft + line.offsetWidth - pin.offsetWidth
-          };
-          var pinCoord = pin.offsetLeft + evt.movementX;
-          if (pinCoord < limitMovementX.min) {
-            pinCoord = limitMovementX.min;
-          }
-          if (pinCoord > limitMovementX.max) {
-            pinCoord = limitMovementX.max;
-          }
-          pin.style.left = pinCoord + 'px';
-          depth.style.width = pinCoord + 'px';
-        };
+        isPinPassed = true;
+      });
+      document.addEventListener('mouseup', function () {
+        isPinPassed = false;
+      });
 
-
-        var onPinMouseup = function () {
-          document.removeEventListener('mousemove', movePin);
-          document.removeEventListener('mousemove', window.formFilter.changeSaturation);
-          document.removeEventListener('mouseup', onPinMouseup);
-        };
-
-        pin.addEventListener('dragstart', function (evt) {
-          evt.preventDefault();
-        });
-        document.addEventListener('mousemove', movePin);
-        document.addEventListener('mousemove', window.formFilter.changeSaturation);
-        document.addEventListener('mouseup', onPinMouseup);
+      document.addEventListener('mousemove', function (evt) {
+        if (isPinPassed === true) {
+          movePin(evt);
+          window.formFilter.changeSaturation();
+        }
       });
     }
   };
