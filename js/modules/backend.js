a0594fe3f92c3f81a90720c1db10db63a831a023
MikleStep <raiving_rabbid@mail.ru>
2020-07-01 15:51:27 +0300

    Реализована подгрузка фотографий с сервера

---
+++ js/modules/backend.js
@@ -0,0 +1,70 @@
+'use strict';
+
+window.backend = (function () {
+  var URL = ' https://javascript.pages.academy/kekstagram';
+  var StatusCode = {
+    OK: 200
+  };
+  var TIMEOUT_IN_MS = 10000;
+
+  return {
+    // save: function (data, onLoad, onError) {
+    //   var xhr = new XMLHttpRequest();
+    //   xhr.responseType = 'json';
+
+    //   xhr.addEventListener('load', function () {
+    //     if (xhr.status === StatusCode.OK) {
+    //       onLoad(xhr.response);
+    //     } else {
+    //       onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+    //     }
+    //   });
+    //   xhr.addEventListener('error', function () {
+    //     onError('Произошла ошибка соединения');
+    //   });
+    //   xhr.addEventListener('timeout', function () {
+    //     onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    //   });
+
+    //   xhr.open('POST', URL);
+    //   xhr.send(data);
+    // },
+
+    loadPhotos: function (onLoad, onError) {
+
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === StatusCode.OK) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+
+      xhr.timeout = TIMEOUT_IN_MS;
+
+      xhr.open('GET', URL + '/data');
+      xhr.send();
+    },
+    error: function (errorMessage) {
+      var node = document.createElement('div');
+      node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: #dc291a;';
+      node.style.position = 'absolute';
+      node.style.left = 0;
+      node.style.right = 0;
+      node.style.fontSize = '22px';
+      node.style.fontFamily = '"Open Sans", "Arial", sans-serif;';
+
+      node.textContent = errorMessage;
+      document.body.insertAdjacentElement('afterbegin', node);
+    }
+  };
+})();
--- js/modules/preview.js
+++ js/modules/preview.js
@@ -3,7 +3,6 @@ window.preview = (function () {
   // Открытие и закрытие модального окна (Big Picture)
   var bigPicture = document.querySelector('.big-picture');
   var bigPictureClose = bigPicture.querySelector('.big-picture__cancel');
-  var bigPictureImg = bigPicture.querySelector('.big-picture__img img');
   document.addEventListener('keydown', window.popup.onPopupEscPress(bigPicture));
   bigPictureClose.addEventListener('click', function () {
     window.popup.toggleModal(bigPicture);
@@ -13,16 +12,16 @@ window.preview = (function () {
     bigPicture.querySelector('.big-picture__img img').setAttribute('src', photo.url);
     bigPicture.querySelector('.likes-count').textContent = photo.likes;
     bigPicture.querySelector('.comments-count').textContent = photo.comments.length;
+    var commentsWrapper = bigPicture.querySelector('.social__comments');
+    commentsWrapper.innerHTML = '';
     for (var k = 0; k < photo.comments.length; k++) {
-      var commentsWrapper = bigPicture.querySelector('.social__comments');
-      var comment = '<li class="social__comment"><img class="social__picture" src = "' + photo.comments[k].avatar + '" alt = "' + photo.comments[k].name + '" width = "35" height = "35" > <p class="social__text">' + photo.comments[k].message + '</p></li>';
+      var comment = '';
+      comment = '<li class="social__comment"><img class="social__picture" src = "' + photo.comments[k].avatar + '" alt = "' + photo.comments[k].name + '" width = "35" height = "35" > <p class="social__text">' + photo.comments[k].message + '</p></li>';
       commentsWrapper.insertAdjacentHTML('afterbegin', comment);
     }
     bigPicture.querySelector('.social__caption').textContent = photo.description;
     return bigPicture;
   };
-
-  renderbigPicture(window.gallery.photosInformation[0]);
 
   document.querySelector('.social__comment-count').classList.add('hidden');
   document.querySelector('.comments-loader').classList.add('hidden');
@@ -30,8 +29,8 @@ window.preview = (function () {
   return {
     addPictureClickHandler: function (thumbnail, photo) {
       thumbnail.addEventListener('click', function () {
+        renderbigPicture(photo);
         window.popup.toggleModal(bigPicture);
-        bigPictureImg.setAttribute('src', photo.url);
       });
     }
   };
