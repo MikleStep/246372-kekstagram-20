ed5a5733133cb18d131f0082371234df957a66f4
MikleStep <raiving_rabbid@mail.ru>
2020-07-17 14:01:47 +0300

    Добавлены фильтры и реализована функция убирания дребезга

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 'use strict';
 
-var successLoadPhotos = function (photo) {
-  window.gallery.renderTemplatePhoto(photo);
-  var bigPictureOpen = document.querySelectorAll('.picture');
-  for (var i = 0; i < bigPictureOpen.length; i++) {
-    window.preview.addPictureClickHandler(bigPictureOpen[i], photo[i]);
-  }
-};
-window.backend.loadPhotos(successLoadPhotos, window.backend.error);
 for (var c = 0; c < window.formFilter.effectsOptions.length; c++) {
   window.formFilter.addThumbnailClickHandler(window.formFilter.effectsOptions[c], window.formFilter.effectsInformation[c]);
 }
