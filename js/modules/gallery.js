9f0a5442cc23865a1539761660b198d09a120ff8
MikleStep <raiving_rabbid@mail.ru>
2020-07-18 17:42:28 +0300

    Убрал лишний slice

--- js/modules/gallery.js
+++ js/modules/gallery.js
@@ -14,7 +14,7 @@ window.gallery = (function (wrapper) {
 
   var successLoadPhotos = function (data) {
     window.gallery.defaultPhotos = data;
-    window.filter.photos = window.gallery.defaultPhotos.slice();
+    window.filter.photos = window.gallery.defaultPhotos;
     window.filter.updatePhotos(window.gallery.defaultPhotos.length, window.filter.hideFilters);
   };
 
